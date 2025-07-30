f33265ee2b3fc2bb31480d9e25dcff317aef307f
spmasabeu <sebastian.palma.masabeu@hotmail.com>
2025-07-30 11:40:04 -0400

    reusable component api

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
 
@@ -7,10 +8,22 @@ import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated with {movieRating}</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
     <StarRating maxRating={10} />
+    <Test color="blue" maxRating={10} />
   </React.StrictMode>
 );
